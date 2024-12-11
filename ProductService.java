127ef06000a717c72a89405694212c93fbb3d014
nirmallyadutta2002 <165792698+nirmallyadutta2002@users.noreply.github.com>
2024-12-12 00:05:39 +0530

    Create  ProductService.java

---
+++ ProductService.java
@@ -0,0 +1,22 @@
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not found"));
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
