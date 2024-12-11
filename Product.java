671f8b05abc57bd7dc02f9870bf43c3d430822d5
nirmallyadutta2002 <165792698+nirmallyadutta2002@users.noreply.github.com>
2024-12-12 00:03:36 +0530

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,13 @@
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
+
+    // Getters and Setters
+}
