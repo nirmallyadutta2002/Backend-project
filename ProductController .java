a0bb02b00a4c4d94aad9190578769d45765e5e60
nirmallyadutta2002 <165792698+nirmallyadutta2002@users.noreply.github.com>
2024-12-12 00:06:29 +0530

    Create ProductController .java

---
+++ ProductController .java
@@ -0,0 +1,27 @@
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+    }
+}
